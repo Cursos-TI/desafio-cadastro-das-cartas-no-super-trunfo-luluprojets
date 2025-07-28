b087cd7029f28c4909a3f3a0c03c592943dcf753
Luana Guimarães <luanxguimar@gmail.com>
2025-07-28 22:37:01 +0000

    criei o return

--- Supertrunfo/supertrunfo.c
+++ Supertrunfo/supertrunfo.c
@@ -1,18 +1,69 @@
 #include <stdio.h>
 int main(){
+    printf("Desafio Trunfo!\n");
 
-printf("Desafio Trunfo\n");
+char Estado1= "Amazonas"
+int Código_da_Carta1 = 01;
+char Nome_da_Cidade1 = "Manaus";
+int População1 = 215000;
+float Área1 = 154114;
+int Números de Pontos Turísticos1 = 5; 
+
+printf("Digite o seu estado:");
+scanf("%s" &estado);
+print("O estado é: %s\n", estado);
+
+printf("Digite o código da carta:");
+scanf("%d", &código da carta);
+printf("O código da carta é: %d\n", código da carta);
+
+printf("Digite o nome da cidade");
+scanf("%s", &cidade);
+printf("O nome da cidade é: %s\n", nome da cidade);
+
+printf("Digite a população:");
+scanf("%f", &população);
+printf("A população é: %f\n", população);
+
+printf("Digite a área:");
+scanf("%e", &área);
+printf("A área é: %e\n", área);
+
+printf("Digite o numero de pontos turísticos:");
+scanf("%d", &números pontos turísticos);
+printf("O número de pontos turísticos é %d\n", números de pontos turísticos);
 
 
+char Estado2 = "Maranhão";
+int Código_da_Carta2 = 02;
+char Nome_da_Cidade2 = "São Luís"
+int População2 = 50.000;
+float Área2 = 1212.46;
+int Números_de_Pontos_Turísticos2 = 4;
 
+printf("Digite o seu estado:");
+scanf("%s" &estado);
+printf("O estado é: %s\n", estado);
 
+printf("Digite o código da carta:");
+scanf("%d", &código da carta);
+printf("O código da carta é: %d\n", código da carta);
 
+printf("Digite o nome da cidade");
+scanf("%s", &cidade);
+printf("O nome da cidade é: %s\n", nome da cidade);
 
+printf("Digite a população:");
+scanf("%f", &população);
+printf("A população é: %f\n" população);
 
+printf("Digite a área:");
+scanf("%e", &área);
+printf("A área é: %e\n", área);
 
+printf("Digite o numero de pontos turísticos:");
+scanf("%d", &números de pontos turísticos);
+printf("O número de pontos turísticos é: %d\n", números de pontos turísticos);
 
-
-
-
-
+return 0;
 }
