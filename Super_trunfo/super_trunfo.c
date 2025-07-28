2c898f7b64aa75693b9576a4ff0a36a79b2986f1
Luana Guimarães <luanxguimar@gmail.com>
2025-07-28 18:20:38 +0000

    criei um char Estado char codigo da carta nome da cidade populçao area numeros de pontos turisticos

---
+++ Super_trunfo/super_trunfo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+char Estado = "A";
+char Código da Carta = [A01];
+char Nome da Cidade = "Amazonas";
+int População = 215.000;
+float Área = 1541.14;
+int Números de Pontos Turísticos = 50; 
+
+
+
+
+}
