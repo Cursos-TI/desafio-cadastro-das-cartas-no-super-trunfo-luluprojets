f5ee70a149ee94a34f97475e78c20193494fc20a
Luana Guimarães <luanxguimar@gmail.com>
2025-07-28 20:33:58 +0000

    carta n 2

--- Super_trunfo/super_trunfo.c
+++ Super_trunfo/super_trunfo.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
 int main(){
     printf("Desafio Trunfo!\n");
-    
-char Estado = 'A';
-char Código_da_Carta [10] = "A01";
-char Nome_da_Cidade = "Amazonas";
+
+char Estado = "Amazonas"
+int Código_da_Carta = 01;
+char Nome_da_Cidade = "Manaus";
 int População = 215000;
 float Área = 154114;
 int Números_de_Pontos_Turísticos = 5; 
 
+printf("Digite o seu estado:");
+scanf("%s" &estado);
+print("O estado é: %s\n", estado);
 
-char Estado = 'B';
-char Código_da_Carta [10] = "B02";
-char Nome_da_Cidade = "Maranhão";
+printf("Digite o código da carta:");
+scanf("%d", &código da carta);
+printf("O código da carta é: %d\n", código da carta)
+
+printf("Digite o nome da cidade");
+scanf("%s", &cidade);
+printf("O nome da cidade é: %s\n", nome da cidade);
+
+printf("Digite a população:");
+scanf("%f", &população);
+printf("A população é: %f\n", população);
+
+printf("Digite a área:");
+scanf("%e", &área);
+printf("A área é: %e\n", área);
+
+printf("Digite o numero de pontos turísticos:");
+scanf("%d", &números pontos turísticos);
+printf("O número de pontos turísticos é %d\n", números de pontos turísticos);
+
+char Estado = "Maranhão"
+int Código_da_Carta= 02;
+char Nome_da_Cidade = "São Luís"
 int População = 50.000;
 float Área = 1212.46
-int Números_de_Pontos_
-Turísticos = 4
+int Números_de_Pontos_Turísticos = 4
 
+rintf("Digite o seu estado:");
+scanf("%s" &estado);
+print("O estado é: %s\n", estado);
+
+printf("Digite o código da carta:");
+scanf("%d", &código da carta);
+printf("O código da carta é: %d\n", código da carta);
+
+printf("Digite o nome da cidade");
+scanf("%s", &cidade);
+printf("O nome da cidade é: %s\n", nome da cidade);
+
+printf("Digite a população:");
+scanf("%f", &população);
+printf("A população é: %f\n" população);
+
+printf("Digite a área:");
+scanf("%e", &área);
+printf("A área é: %e\n", área);
+
+printf("Digite o numero de pontos turísticos:");
+scanf("%d", &números pontos turísticos);
+printf("O número de pontos turísticos é: %d\n", números de pontos turísticos);
+
+return 0;
 }
