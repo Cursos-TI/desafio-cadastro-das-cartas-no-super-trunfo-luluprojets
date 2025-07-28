6c7874390d74412a0c6eeb1e2b451476d1702cce
Luana Guimarães <luanxguimar@gmail.com>
2025-07-28 18:41:45 +0000

    criei um printf

--- Super_trunfo/super_trunfo.c
+++ Super_trunfo/super_trunfo.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 int main(){
+    printf("Desafio Trunfo!\n");
+    
+char Estado = 'A';
+char Código_da_Carta [10] = "A01";
+char Nome_da_Cidade = "Amazonas";
+int População = 215000;
+float Área = 154114;
+int Números_de_Pontos_Turísticos = 5; 
 
-char Estado = "A";
-char Código da Carta = [A01];
-char Nome da Cidade = "Amazonas";
-int População = 215.000;
-float Área = 1541.14;
-int Números de Pontos Turísticos = 50; 
 
-
-
+char Estado = 'B';
+char Código_da_Carta [10] = "B02";
+char Nome_da_Cidade = "Maranhão";
+int População = 50.000;
+float Área = 1212.46
+int Números_de_Pontos_
+Turísticos = 4
 
 }
