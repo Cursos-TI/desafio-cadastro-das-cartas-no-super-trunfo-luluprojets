086b1178fdd58c5efb8af7dc3319a3fa41babce8
Luana Guimarães <luanxguimar@gmail.com>
2025-07-29 19:51:08 +0000

    finalizei

--- Supertrunfo/supertrunfo.c
+++ Supertrunfo/supertrunfo.c
@@ -2,68 +2,75 @@
 int main(){
     printf("Desafio Trunfo!\n");
 
-char Estado1= "Amazonas"
-int Código_da_Carta1 = 01;
-char Nome_da_Cidade1 = "Manaus";
-int População1 = 215000;
-float Área1 = 154114;
-int Números de Pontos Turísticos1 = 5; 
+char Estado1 [09]= "Amazonas"
+int Codigodacarta1 = 01;
+char NomedaCidade1 = "Manaus";
+int Populacao1 = 215000;
+float Area1 = 154114;
+int NumerosdePontosTuristicos1 = 5; 
 
 printf("Digite o seu estado:");
-scanf("%s" &estado);
+scanf("%s" estado);
 print("O estado é: %s\n", estado);
 
 printf("Digite o código da carta:");
-scanf("%d", &código da carta);
+scanf("%d", código da carta);
 printf("O código da carta é: %d\n", código da carta);
 
 printf("Digite o nome da cidade");
-scanf("%s", &cidade);
+scanf("%s", cidade);
 printf("O nome da cidade é: %s\n", nome da cidade);
 
-printf("Digite a população:");
-scanf("%f", &população);
-printf("A população é: %f\n", população);
+printf("Digite a populacao:");
+scanf("%f", populacao);
+printf("A populacao é: %f\n", populacao);
 
-printf("Digite a área:");
-scanf("%e", &área);
-printf("A área é: %e\n", área);
+printf("Digite a area:");
+scanf("%e", area);
+printf("A area é: %e\n", area);
 
-printf("Digite o numero de pontos turísticos:");
-scanf("%d", &números pontos turísticos);
-printf("O número de pontos turísticos é %d\n", números de pontos turísticos);
+printf("Digite o numero de pontos turisticos:");
+scanf("%d", números pontos turisticos);
+printf("O numero de pontos turisticos é %d\n", numeros de pontos turisticos);
 
+printf("Nome do Estado: %s - Código da carta: %d\n", estado, codigo da carta);
+printf("Nome da cidade: %s - Populacao: %f", cidade, populacao);
+printf("Area:%e - Numeros de pontos turisticos:%d", area, numeros de pontos turisticos);
 
-char Estado2 = "Maranhão";
-int Código_da_Carta2 = 02;
-char Nome_da_Cidade2 = "São Luís"
-int População2 = 50.000;
-float Área2 = 1212.46;
-int Números_de_Pontos_Turísticos2 = 4;
+char Estado2 [10]= "Maranhao";
+int Codigodacarta2 = 02;
+char Nomedacidade2 = "Sao Luis"
+int Populacao2 = 50.000;
+float Area2 = 1212.46;
+int Numerosdepontosturisticos2 = 4;
 
 printf("Digite o seu estado:");
-scanf("%s" &estado);
+scanf("%s" estado);
 printf("O estado é: %s\n", estado);
 
-printf("Digite o código da carta:");
-scanf("%d", &código da carta);
-printf("O código da carta é: %d\n", código da carta);
+printf("Digite o codigo da carta:");
+scanf("%d", codigo da carta);
+printf("O codigo da carta é: %d\n", codigo da carta);
 
 printf("Digite o nome da cidade");
 scanf("%s", &cidade);
 printf("O nome da cidade é: %s\n", nome da cidade);
 
-printf("Digite a população:");
-scanf("%f", &população);
-printf("A população é: %f\n" população);
+printf("Digite a populacao:");
+scanf("%f", populacao);
+printf("A população é: %f\n" populacao);
 
-printf("Digite a área:");
-scanf("%e", &área);
-printf("A área é: %e\n", área);
+printf("Digite a area:");
+scanf("%e", area);
+printf("A area é: %e\n", area);
 
-printf("Digite o numero de pontos turísticos:");
-scanf("%d", &números de pontos turísticos);
-printf("O número de pontos turísticos é: %d\n", números de pontos turísticos);
+printf("Digite o numero de pontos turisticos:");
+scanf("%d", numeros de pontos turisticos);
+printf("O numero de pontos turisticos é: %d\n", numeros de pontos turisticos);
+
+printf("Nome do Estado: %s - Código da carta: %d\n", estado, codigo da carta);
+printf("Nome da cidade: %s - Populacao: %f", cidade, populacao);
+printf("Area:%e - Numeros de pontos turisticos:%d", area, numeros de pontos turisticos);
 
 return 0;
 }
